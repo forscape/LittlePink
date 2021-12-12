52cae5ce6776eeb7912b4262ca8c0f6cbc447e8b
forscape <forscape@163.com>
2021-12-12 15:12:13 +0800

    Initial Commit

---
+++ LittlePink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LittlePink
+//
+//  Created by mac on 2021/12/12.
+//  Copyright © 2021 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
